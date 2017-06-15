09df625d946b9ed9196e043807b872139b6f2f48
peboov <peboov@peboovdeMacBook-Pro.local>
2017-06-15 23:52:54 +0800

    Initial commit.

---
+++ ReverseStringSolution.java
@@ -0,0 +1,19 @@
+/**
+ * Created by peboov on 2017/6/15.
+ *
+ * Write a function that takes a string as input and returns the string reversed.
+ *
+ * Example:
+ * Given s = "hello", return "olleh".
+ */
+public class ReverseStringSolution {
+    public String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0, j = chars.length - 1; i < j; i ++, j --) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+        return new String(chars);
+    }
+}
