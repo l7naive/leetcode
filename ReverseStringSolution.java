7b921731ac427027cac01a5c841b29f0b017ee6f
peboov <peboov@gmail.com>
2017-06-19 17:54:25 +0800

    Remove spaces before ++ and --.

--- ReverseStringSolution.java
+++ ReverseStringSolution.java
@@ -9,7 +9,7 @@
 public class ReverseStringSolution {
     public String reverseString(String s) {
         char[] chars = s.toCharArray();
-        for (int i = 0, j = chars.length - 1; i < j; i ++, j --) {
+        for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
             char temp = chars[i];
             chars[i] = chars[j];
             chars[j] = temp;
